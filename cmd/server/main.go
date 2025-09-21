2c0df1b51b74f5cb6058aaf81cfc8ff2765a1ebc
Jari Ahonen <jari.n.ahonen@gmail.com>
2025-09-21 12:59:45 +0300

    test channel

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,10 +6,14 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/jarimus/learn-pub-sub-starter/internal/pubsub"
+	"github.com/jarimus/learn-pub-sub-starter/internal/routing"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
+	// Connect
 	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
 	conn, err := amqp.Dial(rabbitConnString)
 	if err != nil {
@@ -17,6 +21,21 @@ func main() {
 	}
 	defer conn.Close()
 	fmt.Println("Connected successfully...")
+	// New channel
+	testChannel, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("error opening channel: %v", err)
+	}
+
+	pubsub.PublishJSON(
+		testChannel,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{
+			IsPaused: true,
+		},
+	)
+
 	// Waiting for ctrl+c
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/bootdotdev/learn-pub-sub-starter
+module github.com/jarimus/learn-pub-sub-starter
 
 go 1.22.1
 
